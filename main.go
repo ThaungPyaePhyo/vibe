717f5c2bdcdeb569fb1ed5e147c75fd72f1aee1a
thaungpyaephyo <thaungpyaephyo@gmail.com>
2025-06-09 23:54:10 +0700

    create react in golang folder

--- main.go
+++ main.go
@@ -8,8 +8,15 @@ import (
 func main() {
 	config.ConnectDB()
 	router :=  gin.Default()
-	router.GET("/hello", func(c *gin.Context) {
-		c.String(200, "Hello, World!")
+	router.Static("/static", "./frontend/dist")
+	router.NoRoute(func(c *gin.Context) {
+		c.File("./frontend/dist/index.html")
 	})
+	api := router.Group("/api")
+	{
+		api.GET("/hello", func(c *gin.Context) {
+			c.String(200, "Hello, World!")
+		})
+	}
 	router.Run(":8080")
 }
